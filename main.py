2475eee3a49ecfbf87f830fc0225176a30c60ef4
sujay <125623523+Sujay239@users.noreply.github.com>
2026-02-26 19:15:46 +0530

    Change default port from 8000 to 10000

--- main.py
+++ main.py
@@ -94,5 +94,6 @@ from app.ml.bert_classifier import get_model_and_tokenizer
 if __name__ == "__main__":
     import uvicorn
     import os
-    port = int(os.environ.get("PORT", 8000))
-    uvicorn.run("main:app", host="0.0.0.0", port=port, reload=True)
+
+    port = int(os.environ.get("PORT", 10000))
+    uvicorn.run(app, host="0.0.0.0", port=port)
